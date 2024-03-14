9f9a6e4690bf54340535287a6c476e0fe8765e4d
wara <waarapardo@gmail.com>
2024-03-13 21:52:02 -0300

    Alterações na forca

--- forca.py
+++ forca.py
@@ -4,22 +4,35 @@ def jogar():
     print("Bem vindo no jogo da Forca!")
     print("****************************")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()
     enforcou = False
     acertou = False
+    erros = 0
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+
+    print(letras_acertadas)
 
     while(not enforcou and not acertou):
 
         chute = input("Qual letra?")
-        chute = chute.strip()
-        index = 0
-        for letra in palavra_secreta:
-            if chute.upper() == letra.upper():
-                print("encontrei a letra {} na posição {}".format(chute, index))
-            index = index + 1
+        chute = chute.strip().upper()
 
-        print("jogando...")
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                index = index + 1
+        else:
+            erros += 1
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
 
+    if(acertou):
+        print("Você ganhou!")
+    else:
+        print("Você perdeu!")
     print("Fim do jogo")
 
 if(__name__ == "__main__"):
